14b19716cc7911e2134ff86e50c3d99d3bc72024
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-23 17:21:35 +0200

    Add list style

--- src/presentations/catalog_section.js
+++ src/presentations/catalog_section.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CategoriesListContainer from '../containers/categories_list_container';
 import ButtonAddContainer from '../containers/button_add_container'
+import '../stylesheets/catalog_section.css';
 
 function CatalogSection() {
     return (
